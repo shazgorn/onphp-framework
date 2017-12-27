1634c7800d4e33e4bc99bd84e6430f54e64ce1cf
Trofimov Vitaliy <trofimov@pravda.ru>
2017-12-27 14:55:27 +0300

    format LTreeExpression

--- core/Logic/LTreeExpression.class.php
+++ core/Logic/LTreeExpression.class.php
@@ -9,55 +9,55 @@
  *                                                                         *
  ***************************************************************************/
 
-	/**
-	 * Extensive facilities for searching through label trees are provided.
-	 *
-	 * @see http://www.postgresql.org/docs/current/interactive/ltree.html
-	 * @ingroup Logic
-	**/
-	namespace Onphp;
+/**
+ * Extensive facilities for searching through label trees are provided.
+ *
+ * @see http://www.postgresql.org/docs/current/interactive/ltree.html
+ * @ingroup Logic
+ **/
+namespace Onphp;
 
-	final class LTreeExpression extends StaticFactory
-	{
-		const ANCESTOR 		= '@>';
-		const DESCENDANT	= '<@';
-		const MATCH			= '~';
-		const SEARCH		= '@';
-		
-		/**
-		 * Is left argument an ancestor of right (or equal)?
-		 *
-		 * @return \Onphp\BinaryExpression
-		**/
-		public static function ancestor($left, $right)
-		{
-			return new BinaryExpression($left, $right, self::ANCESTOR);
-		}
-		
-		/**
-		 * Is left argument a descendant of right (or equal)?
-		 *
-		 * @return \Onphp\BinaryExpression
-		**/
-		public static function descendant($left, $right)
-		{
-			return new BinaryExpression($left, $right, self::DESCENDANT);
-		}
-		
-		/**
-		 * @return \Onphp\BinaryExpression
-		**/
-		public static function match($ltree, $lquery)
-		{
-			return new BinaryExpression($ltree, $lquery, self::MATCH);
-		}
-		
-		/**
-		 * @return \Onphp\BinaryExpression
-		**/
-		public static function search($ltree, $ltxtquery)
-		{
-			return new BinaryExpression($ltree, $ltxtquery, self::SEARCH);
-		}
-	}
+final class LTreeExpression extends StaticFactory
+{
+    const ANCESTOR      = '@>';
+    const DESCENDANT    = '<@';
+    const MATCH         = '~';
+    const SEARCH        = '@';
+                
+    /**
+     * Is left argument an ancestor of right (or equal)?
+     *
+     * @return \Onphp\BinaryExpression
+     **/
+    public static function ancestor($left, $right)
+    {
+        return new BinaryExpression($left, $right, self::ANCESTOR);
+    }
+                
+    /**
+     * Is left argument a descendant of right (or equal)?
+     *
+     * @return \Onphp\BinaryExpression
+     **/
+    public static function descendant($left, $right)
+    {
+        return new BinaryExpression($left, $right, self::DESCENDANT);
+    }
+                
+    /**
+     * @return \Onphp\BinaryExpression
+     **/
+    public static function match($ltree, $lquery)
+    {
+        return new BinaryExpression($ltree, $lquery, self::MATCH);
+    }
+                
+    /**
+     * @return \Onphp\BinaryExpression
+     **/
+    public static function search($ltree, $ltxtquery)
+    {
+        return new BinaryExpression($ltree, $ltxtquery, self::SEARCH);
+    }
+}
 ?>
